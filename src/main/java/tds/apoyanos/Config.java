802e8ace90049cde1b1455eeb040e53737af4635
Chikitulfo <chikitulfix@gmail.com>
2015-04-16 16:26:27 +0000

    Configuracion DAO

--- src/main/java/tds/apoyanos/Config.java
+++ src/main/java/tds/apoyanos/Config.java
@@ -3,5 +3,5 @@ package tds.apoyanos;
 public class Config {
     public static final long PLAZO_PROYECTO = 2*7*24*60*1000;//2 semanas en milisegundos.
     public static final int VOTOS_NECESARIOS = 50;// Votos necesarios para poder empezar a financiar un proyecto.
-    public static String TipoDAO="umu.tds.dao.H2FactoriaDAO";
+    public static String TipoDAO="tds.apoyanos.persistencia.H2FactoriaDAO";
 }
